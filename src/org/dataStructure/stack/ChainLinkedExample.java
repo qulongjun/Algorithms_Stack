44de9264d6720a8feebe89cf94ea53b559266c2a
瞿龙俊 <154256698@qq.com>
2016-08-21 16:05:35 +0800

    两个有序链表合并

--- src/org/dataStructure/stack/ChainLinkedExample.java
+++ src/org/dataStructure/stack/ChainLinkedExample.java
@@ -1,5 +1,7 @@
 package org.dataStructure.stack;
 
+import java.util.Stack;
+
 import org.junit.Test;
 
 /**
@@ -150,6 +152,142 @@ public class ChainLinkedExample {
 		}
 	}
 
+	/**
+	 * 查找单链表的中间结点
+	 * 
+	 * 设置两个指针，两个指针同时向前走，前面的指针每次走两步，后面的指针每次走一步，
+	 * 前面的指针走到最后一个结点时，后面的指针所指结点就是中间结点，即第（
+	 * n/2+1）个结点。注意链表为空，链表结点个数为1和2的情况。时间复杂度O(n)
+	 * 
+	 * @param head
+	 * @return
+	 */
+	public Node GetMiddleNode(Node head) {
+		if (head == null || head.m_next == null)
+			return head;
+		Node firstNode = head;
+		Node lastNode = head;
+		// 前面指针每次走两步，直到指向最后一个结点，后面指针每次走一步
+		while (firstNode.m_next != null) {
+			firstNode = firstNode.m_next;
+			lastNode = lastNode.m_next;
+			if (firstNode.m_next != null) {
+				firstNode = firstNode.m_next;
+			}
+		}
+		return lastNode;
+	}
+
+	/**
+	 * 从尾到头打印单链表
+	 * 
+	 * 对于这种颠倒顺序的问题，我们应该就会想到栈，后进先出。所以，这一题要么自己使用栈，要么让系统使用栈，也就是递归。注意链表为空的情况
+	 * 
+	 * @param head
+	 */
+	public void ReversePrintListStack(Node head) {
+		Stack<Node> nodeStack = new Stack<Node>();
+		if (head == null)
+			return;
+		Node curNode = head;
+		while (curNode != null) {
+			nodeStack.push(curNode);
+			curNode = curNode.m_next;
+		}
+		while (!nodeStack.isEmpty()) {
+			System.out.println(nodeStack.pop().m_value);
+		}
+	}
+
+	/**
+	 * 从尾到头打印单链表 递归实现
+	 */
+	public void ReversePrintListRec(Node head) {
+		if (head == null) {
+			return;
+		} else {
+			ReversePrintListRec(head.m_next);
+			System.out.println(head.m_value);
+		}
+	}
+
+	/**
+	 * 已知两个单链表pHead1 和pHead2 各自有序，把它们合并成一个链表依然有序
+	 * 
+	 * 这个类似归并排序。尤其注意两个链表都为空，和其中一个为空时的情况。只需要O（1）的空间。时间复杂度为O（max(len1, len2)）
+	 * 
+	 * 
+	 * @param head1
+	 * @param head2
+	 * @return
+	 */
+	public Node MergeSortedList(Node head1, Node head2) {
+		if (head1 == null)
+			return head2;
+		if (head2 == null)
+			return head2;
+
+		Node mergeNode = null;
+		if (head1.m_value < head2.m_value) { // 选择一个根节点加入，找到mergeNode的起始结点
+			mergeNode = head1;
+			head1 = head1.m_next;
+			mergeNode.m_next = null;
+		} else {
+			mergeNode = head2;
+			head2 = head2.m_next;
+			mergeNode.m_next = null;
+		}
+
+		Node mergeCur = mergeNode;
+		while (head1 != null && head2 != null) {
+			if (head1.m_value < head2.m_value) {
+				mergeCur.m_next = head1; // 此处将mergeCur链表后面将包括head1在内的之后所有的链表加入mergeCur中
+				head1 = head1.m_next; // head1指向之后的节点
+				mergeCur = mergeCur.m_next;// 将mergeCur之后的一个结点信息赋给mergeCur
+				mergeCur.m_next = null;// 断开mergeCur与之后的联系
+
+			} else {
+				mergeCur.m_next = head2;
+				head2 = head2.m_next;
+				mergeCur = mergeCur.m_next;
+				mergeCur.m_next = null;
+			}
+		}
+
+		if (head1 != null) {
+			mergeCur.m_next = head1;
+		} else if (head2 != null) {
+			mergeCur.m_next = head2;
+		}
+		return mergeNode;
+	}
+
+	/**
+	 * 已知两个单链表pHead1 和pHead2 各自有序，把它们合并成一个链表依然有序 递归解法
+	 * 
+	 * （1）若head1或者head2为空，返回另一个
+	 * （2）比较head1和head2的值，若head1大，则head1被加入新链表，然后比较head1的next和head2大小，反之亦然。
+	 * 
+	 * @param head1
+	 * @param head2
+	 * @return
+	 */
+	public Node MergeSortedListRec(Node head1, Node head2) {
+		if (head1 == null)
+			return head2;
+		if (head2 == null)
+			return head1;
+		Node mergedNode = null;
+		if (head1.m_value < head2.m_value) {
+			mergedNode = head1;
+			mergedNode.m_next =MergeSortedListRec(head1.m_next, head2);
+		} else {
+			mergedNode = head2;
+			mergedNode.m_next= MergeSortedListRec(head1, head2.m_next);
+		}
+		return mergedNode;
+	}
+
 	@Test
 	public void testNode() throws Exception {
 		Node node10 = new Node(10, null);
@@ -162,13 +300,29 @@ public class ChainLinkedExample {
 		Node node3 = new Node(3, node4);
 		Node node2 = new Node(2, node3);
 		Node node1 = new Node(1, node2);
+
+		Node node20 = new Node(20, null);
+		Node node19 = new Node(19, node20);
+		Node node18 = new Node(18, node19);
+		Node node17 = new Node(15, node18);
+		Node node16 = new Node(11, node17);
+		Node node15 = new Node(9, node16);
+		Node node14 = new Node(8, node15);
+		Node node13 = new Node(4, node14);
+		Node node12 = new Node(2, node13);
+		Node node11 = new Node(1, node12);
+
 		// System.out.println(GetListLength(node1));
 		// printNode(ReverseList(node1));
 		// printNode(ReverseListRec(node1));
 		// System.out.println(ReGetKthNode(node1, 5).m_value);
-		//System.out.println(ReGetKthNode2(node1, 4).m_value);
-		
-		ReGetKthNodeRec(node1,4);
+		// System.out.println(ReGetKthNode2(node1, 4).m_value);
+
+		// ReGetKthNodeRec(node1, 4);
+		// System.out.println(GetMiddleNode(node1).m_value);
+		// ReversePrintListStack(node1);
+		// ReversePrintListRec(node1);
+		printNode(MergeSortedListRec(node1, node11));
 	}
 }
 
